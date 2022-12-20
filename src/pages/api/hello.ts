3259303fbbec91f56862efa817228ef610e62e49
oji3 <oji3xx@gmail.com>
2022-12-20 17:17:55 +0800

    fix

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Application, Container, Graphics } from '@pixi/node'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: 'John Doe' })
+  const app = new Application()
+  const container = new Container()
+  app.stage.addChild(container)
+  const g = new Graphics().beginFill(0xffffff).drawRect(0, 0, 200, 200).endFill()
+  container.addChild(g)
+
+  const canvas = app.renderer.plugins.extract.canvas(container)
+  const buf = canvas.toBuffer('image/png')
+  res.setHeader('Content-Type', 'image/png')
+  res.setHeader('Content-Length', buf.length)
+  res.status(200).end(buf, 'binary')
 }
